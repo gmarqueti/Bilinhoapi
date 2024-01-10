49a10d98b2d9cf1503abbcd83d9fd51037d1fb4f
gmarqueti <bielmarqueti@gmail.com>
2024-01-10 15:04:57 -0300

    feat:  new version bill_create

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -13,30 +13,33 @@ class Billing < ApplicationRecord
     after_create :bill_create
 
     private 
+
+  def bill_create
+    bill_value = total_value/installments #total de faturas
+    today_day = Date.today.day
     
-    def bill_create
-        bill_value = total_value/installments #total de faturas
-        today_day = Date.today.day
-        due_day = self.due_day.to_i #garantindo que due_day é um inteiro
+    due_day = due_day.to_i #garantindo que due_day é um inteiro
+    new_due_day =  #pegando o dia certo
+    
 
-        if due_day <= today_day
-            due_day = Date.new(Date.today.year, Date.today.month + 1, due_day)
-        else
-            due_day = Date.new(Date.today.year, Date.today.month, due_day)
-        end
-
-        while Bill.find_by(due_date: due_day, billing_id: id).present?
-            due_day = due_day.next_month  # Se encontrar data de vencimento igual a due day, atualiza para o próximo mês
-        end
-
-        Bill.create(value:bill_value,due_date:due_day,billing_id: id) #cria a primeira fatura 
-        self.installments -= 1
-
-        i= 0
-        while i < self.installments do #estrutura de repetição para criar a quantidade de faturas restantes
-            due_day = due_day.next_month
-            Bill.create(value:bill_value,due_date:due_day,billing_id: id)
-            i += 1
-        end
+    if due_day <= today_day
+      due_date = Date.new(Date.today.year, Date.today.month + 1, due_day)
+    else
+      due_date = Date.new(Date.today.year, Date.today.month, due_day)
     end
+
+    while Bill.find_by(due_date: due_date, billing_id: id).present?
+      due_date = due_date.next_month  # Se encontrar data de vencimento igual a due day, atualiza para o próximo mês
+    end
+
+    Bill.create(value:bill_value, due_date:due_date, billing_id: id) #cria a primeira fatura 
+    installments -= 1
+
+    installments.each do |installment|
+      due_date = due_date.next_month
+      Bill.create(value:bill_value,due_date:due_date,billing_id: id)
+    end
+  end
 end
+
+
